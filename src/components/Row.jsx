5ed203aeeb5dfb76f3d7c8d4230b80c8aeceddf6
nawalabdi <nawalabdi03@gmail.com>
2023-05-02 14:41:36 -0400

    added scroll feeature on rows

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect,  useState } from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import {MdChevronLeft, MdChevronRight,  } from 'react-icons/md';
+
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like,setLike]= useState(false)
+ 
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -16,12 +17,14 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+      <MdChevronLeft className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
+        <div id={"slider"} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {movies.map((item, id) => (
            <Movie key={id} item={item} /> 
           ))}
         </div>
+        <MdChevronRight className="bg-white  right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
       </div>
     </>
   );
