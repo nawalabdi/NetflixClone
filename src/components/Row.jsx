72e8d896dc40d7764d50fe92ff9f0a8e23a0abf1
nawalabdi <nawalabdi03@gmail.com>
2023-05-02 21:37:15 -0400

    setup firebase and uauthentication

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import {MdChevronLeft, MdChevronRight,  } from 'react-icons/md';
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
   const [movies, setMovies] = useState([]);
- 
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -14,17 +12,37 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
-      <MdChevronLeft className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
-        <div id={"slider"} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-           <Movie key={id} item={item} /> 
+            <Movie key={id} item={item} />
           ))}
         </div>
-        <MdChevronRight className="bg-white  right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" size={40} />
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white  right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
