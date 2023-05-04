e664d755ee8aed03a1e0d1f129237ef8281b02f0
nawalabdi <nawalabdi03@gmail.com>
2023-05-03 20:42:04 -0400

    added logout functionality, redirects to homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
+import { async } from "@firebase/util";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate;
   // console.log(user.email)
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="flex items-center justify-between z-[100]  w-full absolute">
@@ -18,11 +29,10 @@ const Navbar = () => {
           <Link to="/account">
             <button className="text-white pr-4 "> Account </button>
           </Link>
-          <Link to="/signup">
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-         Logout
-            </button>
-          </Link>
+
+          <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+            Logout
+          </button>
         </div>
       ) : (
         <div>
