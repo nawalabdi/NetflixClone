99d792c299df7fcf468f4db0a7dd1d41cc9b5c57
nawalabdi <nawalabdi03@gmail.com>
2023-05-04 17:38:46 -0400

    use firebase to add specific users to database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import { AuthContextProvider } from "./context/AuthContext";
-import Login from "./pages/Login";
-import Account from "./pages/Account";
-import Signup from "./pages/Signup";
-import ProtectedRoute from "./components/ProtectedRoute";
-
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -14,12 +13,17 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login/>} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="account" element={<ProtectedRoute>
-            <Account />
-          </ProtectedRoute>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </>
