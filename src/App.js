9a15e2f09b077940c40c88ec19216b4f64fbb971
nawalabdi <nawalabdi03@gmail.com>
2023-05-04 16:25:18 -0400

    completed authentication and added protected route on account page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Account from "./pages/Account";
 import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="login" element={<Login/>} />
           <Route path="signup" element={<Signup />} />
-          <Route path="account" element={<Account />} />
+          <Route path="account" element={<ProtectedRoute>
+            <Account />
+          </ProtectedRoute>} />
         </Routes>
       </AuthContextProvider>
     </>
