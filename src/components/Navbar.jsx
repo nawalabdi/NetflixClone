b2af328e63c90fba9273c2809c6d8e080e8f5d42
nawalabdi <nawalabdi03@gmail.com>
2023-05-03 20:37:14 -0400

    add Account and Logout styling to Navbar depending on if the user is logged in or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  // console.log(user.email)
+
   return (
     <div className="flex items-center justify-between z-[100]  w-full absolute">
       <Link to="/">
@@ -9,16 +13,29 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="text-white pr-4 "> Sign In </button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-            Sign Up
-          </button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4 "> Account </button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+         Logout
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4 "> Sign In </button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
